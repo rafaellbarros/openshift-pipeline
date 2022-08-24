67c78261264c03b9db1a060059fed8fb059e956f
Rafael <rafaelbarros.df@gmail.com>
2022-08-24 01:10:26 -0300

    :construction: Create worldName

--- src/main/java/br/com/rafaellbarros/openshiftpipeline/rest/HelloRest.java
+++ src/main/java/br/com/rafaellbarros/openshiftpipeline/rest/HelloRest.java
@@ -4,13 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * created by:
  *
- * @author rafael barros for DevDusCorre <rafaelbarros.softwareengineer@gmail.com> on 23/08/2022
+ * @author rafael barros for DevDusCorre <rafaelbarros.softwareengineer@gmail.com> on 19/08/2022
  */
 
 @RestController
@@ -21,5 +22,11 @@ public class HelloRest {
     @GetMapping(path = "/world", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> world() {
         return ResponseEntity.ok("Hello World OpenShift Pipepline!");
+    }
+
+    @GetMapping(path = "/world/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> worldName(@PathVariable String name) {
+        String reqName = name == null || name == "" ? "Xablau" : name;
+        return ResponseEntity.ok("Hello World OpenShift Pipepline " + reqName.toUpperCase()+ "!");
     }
 }
